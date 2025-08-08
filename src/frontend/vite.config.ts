7d07b5d7aa4b858d5735b45fbdfb75f7243f95ee
jaowei <jaowei.8@gmail.com>
2025-08-08 14:53:02 +0800

    add react compiler

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,5 +4,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
