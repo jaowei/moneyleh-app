e8549c090a32cb6210b5821a764e771c5157853c
jaowei <jao.wei.teo@sap.com>
2025-09-25 15:55:50 +0800

    setup auth for frontend & backend

--- src/frontend/src/context/auth.tsx
+++ src/frontend/src/context/auth.tsx
@@ -1,72 +1,68 @@
-import React, {createContext, useContext, useState} from "react";
-import { authClient } from "../lib/auth-client";
+import React, {createContext, useContext} from "react";
+import {authClient} from "../lib/auth-client";
 
 interface User {
-  email?: string;
-  name?: string
+    email?: string;
+    name?: string;
+    id?: string;
 }
- 
+
 export interface AuthState {
-  isAuthenticated: boolean;
-  user: User | undefined;
-  login: (email: string, password: string) => Promise<void>;
-  logout: () => void;
+    isAuthenticated: boolean;
+    user: User | undefined;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => void;
 }
 
 const AuthContext = createContext<AuthState | undefined>(undefined);
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const { data, isPending } = authClient.useSession();
-  //TODO: handle authenticated by session
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
+export function AuthProvider({children}: { children: React.ReactNode }) {
+    const {data, isPending} = authClient.useSession();
 
-  // Show loading state while checking auth
-  if (isPending) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        Loading...
-      </div>
-    );
-  }
+    // Show loading state while checking auth
+    if (isPending) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                Loading...
+            </div>
+        );
+    }
 
-  const login = async (email: string, password: string) => {
-      const {error: authError} = await authClient.signIn.email({
-         email,
-         password,
-          rememberMe: true
-      })
+    const login = async (email: string, password: string) => {
+        const {error: authError} = await authClient.signIn.email({
+            email,
+            password,
+            rememberMe: true
+        })
         if (authError) {
             throw authError
         }
-        setIsAuthenticated(true)
-  };
+    };
 
-  const logout = async () => {
-     await authClient.signOut()
-      setIsAuthenticated(false)
-  };
+    const logout = async () => {
+        await authClient.signOut()
+    };
 
-  console.log(isAuthenticated)
-  return (
-    <AuthContext.Provider
-      value={{
-        isAuthenticated,
-        user: { email: data?.user?.email, name: data?.user?.name },
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                isAuthenticated: data?.session?.expiresAt ? data.session.expiresAt > new Date() : false,
+                user: {email: data?.user?.email, name: data?.user?.name, id: data?.user?.id},
+                login,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 // TODO: determine if fast refresh is important and affects anything
 // eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,23 +1,29 @@
-import { betterAuth } from "better-auth";
-import { Database } from "bun:sqlite";
+import {betterAuth} from "better-auth";
+import {Database} from "bun:sqlite";
 import {anonymous, magicLink} from "better-auth/plugins";
-import { resend } from "./email";
+import {resend} from "./email";
 
 export const auth = betterAuth({
-  database: new Database("./moneyleh-auth.db"),
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [
-    magicLink({
-      sendMagicLink: async ({ email, url }) => {
-        await resend.emails.send({
-          from: "onboarding@resend.dev",
-          to: email,
-          subject: "Sign in to moneyleh",
-          html: `<p>Sign in to moneyleh: ${url}</p>`,
-        });
-      },
-    }),
-  ],
+    database: new Database("./moneyleh-auth.db"),
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [
+        magicLink({
+            sendMagicLink: async ({email, url}) => {
+                await resend.emails.send({
+                    from: "onboarding@resend.dev",
+                    to: email,
+                    subject: "Sign in to moneyleh",
+                    html: `<p>Sign in to moneyleh: ${url}</p>`,
+                });
+            },
+        }),
+    ],
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60 * 24
+        }
+    }
 });
